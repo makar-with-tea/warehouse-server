49c704f687417e45146e1fa7bbbd8a9b1716894c
Мария <maramakarevich@mail.ru>
2025-02-23 22:44:40 +0300

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,11 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 export const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log(`${req.method} ${req.url}`);
     console.log('Request Body:', req.body);
-    
+
     const originalSend = res.send;
   
     res.send = function (body) {
-      console.log(`${req.method} ${req.url} ${res.statusCode}`);
+      console.log(`Response code: ${res.statusCode}`);
       console.log('Response Body:', body);
       return originalSend.apply(this, arguments);
     };
